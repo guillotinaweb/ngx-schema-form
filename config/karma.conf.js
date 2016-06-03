243a8fd7bacea8fa148d677de8830625ee12bf5e
Frank Bessou <fbessou@free.fr>
2016-06-03 16:10:15 +0200

    Enabled watch mode in Karma.

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function(config) {
      *
      * we are building the test environment in ./spec-bundle.js
      */
-    files: [ { pattern: './config/spec-bundle.js', watched: false } ],
+    files: [ { pattern: './config/spec-bundle.js', watched: true} ],
 
     /*
      * preprocess matching files before serving them to the browser
@@ -69,7 +69,7 @@ module.exports = function(config) {
     logLevel: config.LOG_INFO,
 
     // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: false,
+    autoWatch: true,
 
     /*
      * start these browsers
@@ -84,7 +84,7 @@ module.exports = function(config) {
      * Continuous Integration mode
      * if true, Karma captures browsers, runs the tests and exits
      */
-    singleRun: true
+    singleRun: false
   });
 
 };
