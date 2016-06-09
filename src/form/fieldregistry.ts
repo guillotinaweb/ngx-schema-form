71c6e5b2563568c4383f6a4383bd28c8df4feaa5
Frank Bessou <fbessou@free.fr>
2016-06-09 10:50:17 +0200

    Added default field in field registry and corresponding tests.

--- src/form/fieldregistry.ts
+++ src/form/fieldregistry.ts
@@ -1,22 +1,26 @@
 import {StringField} from "./fields/string"
 import {IntegerField} from "./fields/integer"
 import {TextLineField} from "./fields/textline"
+import {BaseField} from "./fields/base"
 
 export class FieldRegistry {
 	private fieldTypes: {[type:string] : any} = {};
 
-	constructor(){
+	constructor(private defaultFieldType: any = StringField){
 		this.registerFieldType("string", StringField)
 		this.registerFieldType("integer", IntegerField)
 		this.registerFieldType("textline", TextLineField)
 	}
 
 	registerFieldType(type: string, field: any){
-		this.fieldTypes[type]=field;
+		this.fieldTypes[type] = field;
 	}
 
 	getFieldType(type: string): any{
-		return this.fieldTypes[type];
+		if(this.fieldTypes.hasOwnProperty(type)){
+			return this.fieldTypes[type];
+		}
+		return this.defaultFieldType;
 	}
 
 	getFieldTypes(): any{
