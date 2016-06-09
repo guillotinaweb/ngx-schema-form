e8a18442171282587e6c3261c954f946bff8e676
Frank Bessou <fbessou@free.fr>
2016-06-09 10:50:17 +0200

    Added default field in field registry and corresponding tests.

--- src/form/fieldfactory.spec.ts
+++ src/form/fieldfactory.spec.ts
@@ -8,11 +8,20 @@ import {
 } from '@angular/core/testing';
 
 import {StringField} from "./fields/string"
+import {IntegerField} from "./fields/integer"
+import {TextLineField} from "./fields/textline"
+
 import {FieldRegistry} from "./fieldregistry";
 
 describe("FieldRegistry",() => {
+	let STRING_TYPE = "string";
+	let INT_TYPE = "integer";
+	let TEXTLINE_TYPE = "textline";
+
 	let A_NOT_REGISTERED_TYPE = "FOOBARSTRING";
-	let THE_DEFAULT_FIELD_TYPE = new class{};
+	let THE_DEFAULT_FIELD_TYPE = class{};
+	let THE_TYPE = Date;
+	let THE_FIELD_TYPE = class {};
 
 	let registry : FieldRegistry;
 
@@ -20,16 +29,35 @@ describe("FieldRegistry",() => {
 		registry = new FieldRegistry();
 	});
 
+	it("should be initialized with primitives fields", () => {
+		let stringField = registry.getFieldType(STRING_TYPE);
+		let integerField = registry.getFieldType(INT_TYPE);
+		let textlineField = registry.getFieldType(TEXTLINE_TYPE);
+
+		expect(stringField).toBe(StringField);
+		expect(integerField).toBe(IntegerField);
+		expect(textlineField).toBe(TextLineField);
+	});
+
 	it("should return a default field if there is no matching string in fieldTypes",() => {
 		let fieldType = registry.getFieldType(A_NOT_REGISTERED_TYPE);
-		
+
 		expect(fieldType).not.toBe(null);
 	});
 
-	it("should return the field provided in constructor if there is no matching string in fieldTypes",() => {
-		let registry = new FieldRegistry(THE_DEFAULT_FIELD_TYPE);
+	it("should return the field type set when there is no matching type registered",() => {
+		registry.setDefaultFieldType(THE_DEFAULT_FIELD_TYPE);
+
 		let fieldType = registry.getFieldType(A_NOT_REGISTERED_TYPE);
-		
+
 		expect(fieldType).toBe(THE_DEFAULT_FIELD_TYPE);
+	});
+
+	it("should register a field type",() => {
+		registry.registerFieldType(THE_TYPE,THE_FIELD_TYPE);
+
+		let fieldType = registry.getFieldType(THE_TYPE);
+
+		expect(fieldType).toBe(THE_FIELD_TYPE);
 	});
 });
