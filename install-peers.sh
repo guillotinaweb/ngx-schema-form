b92c7a707797ae7099aa35c676c4d7ad5cce1f81
Frank Bessou <fbessou@free.fr>
2016-08-04 11:29:06 +0200

    Test travis custom install.

---
+++ install-peers.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+npm install @angular/forms @angular/common @angular/compiler @angular/platform-browser @angular/platform-browser-dynamic @angular/platform-server @angular/core rxjs@5.0.0-beta.6 zone.js
