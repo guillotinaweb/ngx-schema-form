a3b739ef4378a573df94e8c96d9baf88a7ff1111
Eric BREHAULT <ebrehault@gmail.com>
2016-09-27 22:50:12 +0200

    upgrade to ng2 2.0.0 final

--- demo/app.component.ts
+++ demo/app.component.ts
@@ -1,83 +1,96 @@
-import { Component, Directive, ElementRef, NgZone, Renderer, ViewEncapsulation,ViewContainerRef,ComponentMetadata} from "@angular/core";
-import { FormComponent, WidgetRegistry, Validator, DefaultWidgetRegistry} from "../src";
+import {
+    Component,
+    Directive,
+    ElementRef,
+    NgZone,
+    Renderer,
+    ViewEncapsulation,
+    ViewContainerRef
+} from "@angular/core";
+import {
+    FormComponent,
+    WidgetRegistry,
+    Validator,
+    DefaultWidgetRegistry
+} from "../src";
 
 @Component({
-	selector: "schema-form-demo-app",
-	template: require("./app.component.html"),
-	styleUrls: ["demo/app.scss"],
-	encapsulation: ViewEncapsulation.None,
-	providers: [{provide: WidgetRegistry, useClass: DefaultWidgetRegistry}]
+    selector: "schema-form-demo-app",
+    template: require("./app.component.html"),
+    styleUrls: ["demo/app.scss"],
+    encapsulation: ViewEncapsulation.None,
+    providers: [{provide: WidgetRegistry, useClass: DefaultWidgetRegistry}]
 })
 export class AppComponent {
 
-	private schema:any;
-	private model:any;
-	private fieldValidators : { [fieldId:string]: Validator} = {};
-	private actions = {}
+    private schema:any;
+    private model:any;
+    private fieldValidators : { [fieldId:string]: Validator} = {};
+    private actions = {}
 
-	constructor(registry: WidgetRegistry) {
+    constructor(registry: WidgetRegistry) {
 
-		this.schema = (() => {
-			try {
-				return require("./sampleschema.json");
-			} catch (e) {
-				console.log(e);
-			}
-		})();
-		this.model = (() => {
-			try {
-				return require("./samplemodel.json");
+        this.schema = (() => {
+            try {
+                return require("./sampleschema.json");
+            } catch (e) {
+                console.log(e);
+            }
+        })();
+        this.model = (() => {
+            try {
+                return require("./samplemodel.json");
 
-			} catch (e) {
-				console.log(e);
-			}
-		})();
+            } catch (e) {
+                console.log(e);
+            }
+        })();
 
-		this.fieldValidators["/bornOn"] = (value, property, form) => {
-			let errors = null;
-			let dateArr = value.split("-");
-			if (dateArr.length === 3) {
-				let now = new Date();
-				let min = new Date(now.getFullYear()-100, now.getMonth(), now.getDay()).getTime();
-				let max = new Date().getTime();
-				let born = new Date(dateArr[0],dateArr[1]-1,dateArr[2]).getTime();
-				if (born < min || born > max ) {
-					errors = [{"bornOn": {"expectedValue": ">today - 100 && < today", "actualValue":value}}];
-				}
-			}
-			return errors;
-		};
+        this.fieldValidators["/bornOn"] = (value, property, form) => {
+            let errors = null;
+            let dateArr = value.split("-");
+            if (dateArr.length === 3) {
+                let now = new Date();
+                let min = new Date(now.getFullYear()-100, now.getMonth(), now.getDay()).getTime();
+                let max = new Date().getTime();
+                let born = new Date(dateArr[0],dateArr[1]-1,dateArr[2]).getTime();
+                if (born < min || born > max ) {
+                    errors = [{"bornOn": {"expectedValue": ">today - 100 && < today", "actualValue":value}}];
+                }
+            }
+            return errors;
+        };
 
-		this.fieldValidators["/promotion"] = (value, property, form) => {
-			if (value === "student") {
-				let bornOn = form.getProperty("/bornOn");
-				if (bornOn.valid) {
-					let date = bornOn.value.split('-');
-					let validYear = new Date().getFullYear() -17;
-					try{
-						let actualYear = parseInt(date[0]);
-						if (actualYear < validYear) {
-							return null;
-						}
-						return [{"promotion": {"bornOn": {"expectedValue": "year<"+validYear, "actualValue": actualYear}}}];
-					} catch (e) { }
-				}
-				return [{"promotion": {"bornOn": {"expectedFormat": "date","actualValue": bornOn.value}}}];
-			}
-			return null;
-		}
+        this.fieldValidators["/promotion"] = (value, property, form) => {
+            if (value === "student") {
+                let bornOn = form.getProperty("/bornOn");
+                if (bornOn.valid) {
+                    let date = bornOn.value.split('-');
+                    let validYear = new Date().getFullYear() -17;
+                    try{
+                        let actualYear = parseInt(date[0]);
+                        if (actualYear < validYear) {
+                            return null;
+                        }
+                        return [{"promotion": {"bornOn": {"expectedValue": "year<"+validYear, "actualValue": actualYear}}}];
+                    } catch (e) { }
+                }
+                return [{"promotion": {"bornOn": {"expectedFormat": "date","actualValue": bornOn.value}}}];
+            }
+            return null;
+        }
 
-		this.actions["alert"] = (property, options) => {
-			alert(JSON.stringify(property.value));
-		}
+        this.actions["alert"] = (property, options) => {
+            alert(JSON.stringify(property.value));
+        }
 
-		this.actions["reset"] = (form, options) => {
-			form.reset();
-		}
+        this.actions["reset"] = (form, options) => {
+            form.reset();
+        }
 
-		this.actions["addItem"] =(property, parameters) => {
-			property.addItem(parameters.value);
-		}
-	}
+        this.actions["addItem"] =(property, parameters) => {
+            property.addItem(parameters.value);
+        }
+    }
 
 }
