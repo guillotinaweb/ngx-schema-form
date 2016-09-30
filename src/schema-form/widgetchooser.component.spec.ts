9c771391b950b59944e9e209188eb6d97aae567a
Sbats <simon.bats12@gmail.com>
2016-09-30 17:36:57 +0200

    Fix widget chooser specs

--- src/schema-form/widgetchooser.component.spec.ts
+++ src/schema-form/widgetchooser.component.spec.ts
@@ -1,14 +1,28 @@
+import { ChangeDetectorRef } from "@angular/core";
 import { TestBed } from "@angular/core/testing";
 
 import { WidgetFactory } from "./widgetfactory";
+import { WidgetRegistry } from "./widgetregistry";
+
 import { WidgetChooserComponent } from "./widgetchooser.component";
 
 describe("WidgetChooserComponent", () => {
 
-  let factory: WidgetFactory;
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [WidgetChooserComponent],
+      providers: [
+        WidgetFactory,
+        WidgetRegistry,
+        ChangeDetectorRef
+      ]
+    });
+  });
 
-  it("should create a widget", () => {
-    TestBed.createComponent(WidgetChooserComponent);
+  it("should create a widget which contain an instance", () => {
+    let fixture = TestBed.createComponent(WidgetChooserComponent);
+    let widgetChooserComponent = fixture.debugElement.componentInstance;
+    expect(widgetChooserComponent).toBeDefined();
   });
 
   xit("should put the widget returned by the factory in the DOM", () => {
