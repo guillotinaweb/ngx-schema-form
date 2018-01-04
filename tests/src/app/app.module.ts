51800b1657be1a9f24d2794a15ebad5165fb5bf7
daniele-pecora <daniele.pecora@hsh-berlin.com>
2018-01-04 17:28:06 +0100

    fixes #180 Updated Test app Update README

--- tests/src/app/app.module.ts
+++ tests/src/app/app.module.ts
@@ -13,7 +13,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    SchemaFormModule,
+    SchemaFormModule.forRoot(),
     HttpModule
   ],
   providers: [],
