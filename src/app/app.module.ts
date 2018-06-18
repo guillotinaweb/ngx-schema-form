9345025eb26faa4a49dc31f9b8f8ea29b5a75683
Eric Brehault <ebrehault@gmail.com>
2018-06-18 18:50:58 +0200

    demo: allow to load schema from url

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
 
 import { SchemaFormModule, SchemaValidatorFactory, ZSchemaValidatorFactory } from 'ngx-schema-form';
 import { AppComponent } from './app.component';
@@ -14,7 +14,7 @@ import { AppComponent } from './app.component';
     BrowserModule,
     FormsModule,
     SchemaFormModule,
-    HttpModule
+    HttpClientModule,
   ],
   providers: [
     {
