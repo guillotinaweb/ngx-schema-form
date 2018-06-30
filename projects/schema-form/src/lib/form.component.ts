a652f458813cd0f119b2481924bfbbed2eda323c
Juan Manuel Verges <juanmanuel@verges.com.do>
2018-06-29 21:52:03 -0400

    Implemented @angular ControlValueAccessor on FormComponent

--- projects/schema-form/src/lib/form.component.ts
+++ projects/schema-form/src/lib/form.component.ts
@@ -6,6 +6,7 @@ import {
   Input,
   Output
 } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 import {Action} from './model/action';
 import {ActionRegistry} from './model/actionregistry';
@@ -41,9 +42,14 @@ export function useFactory(schemaValidatorFactory, validatorRegistry) {
       deps: [SchemaValidatorFactory, ValidatorRegistry]
     },
     TerminatorService,
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: FormComponent,
+      multi: true
+    }
   ]
 })
-export class FormComponent implements OnChanges {
+export class FormComponent implements OnChanges, ControlValueAccessor {
 
   @Input() schema: any = null;
 
@@ -62,15 +68,40 @@ export class FormComponent implements OnChanges {
   @Output() onErrorChange = new EventEmitter<{ value: any[] }>();
 
   @Output() onErrorsChange = new EventEmitter<{value: any}>();
-  
+
   rootProperty: FormProperty = null;
 
-  constructor(private formPropertyFactory: FormPropertyFactory,
-              private actionRegistry: ActionRegistry,
-              private validatorRegistry: ValidatorRegistry,
-              private cdr: ChangeDetectorRef,
-              private terminator: TerminatorService,) {
+  private onChangeCallback: any;
+
+  constructor(
+    private formPropertyFactory: FormPropertyFactory,
+    private actionRegistry: ActionRegistry,
+    private validatorRegistry: ValidatorRegistry,
+    private cdr: ChangeDetectorRef,
+    private terminator: TerminatorService
+  ) { }
+
+  writeValue(obj: any) {
+    if (this.rootProperty) {
+      this.rootProperty.reset(obj, false);
+    }
   }
+
+  registerOnChange(fn: any) {
+    this.onChangeCallback = fn;
+    if (this.rootProperty) {
+      this.rootProperty.valueChanges.subscribe(
+        this.onValueChanges.bind(this)
+      );
+    }
+  }
+
+  // TODO implement
+  registerOnTouched(fn: any) {
+  }
+
+  // TODO implement
+  // setDisabledState(isDisabled: boolean)?: void
 
   ngOnChanges(changes: any) {
     if (changes.validators) {
@@ -89,36 +120,35 @@ export class FormComponent implements OnChanges {
       if (!changes.schema.firstChange) {
         this.terminator.destroy();
       }
+
       SchemaPreprocessor.preprocess(this.schema);
       this.rootProperty = this.formPropertyFactory.createProperty(this.schema);
+      if (this.model) {
+        // this.rootProperty.reset(this.model, false);
+      }
 
-      this.rootProperty.valueChanges.subscribe(value => {
-        if(this.modelChange.observers.length > 0) { // two way binding is used
-          if (this.model) {
-            Object.assign(this.model, value);
-          } else {
-            this.model = value;
-          }
-          this.modelChange.emit(value);
-        }
-        this.onChange.emit({value: value});
-      });
+      this.rootProperty.valueChanges.subscribe(
+        this.onValueChanges.bind(this)
+      );
+
       this.rootProperty.errorsChanges.subscribe(value => {
         this.onErrorChange.emit({value: value});
         this.isValid.emit(!(value && value.length));
       });
+
     }
 
     if (this.schema && (changes.model || changes.schema )) {
       this.rootProperty.reset(this.model, false);
       this.cdr.detectChanges();
     }
+
   }
 
   private setValidators() {
     this.validatorRegistry.clear();
     if (this.validators) {
-      for (let validatorId in this.validators) {
+      for (const validatorId in this.validators) {
         if (this.validators.hasOwnProperty(validatorId)) {
           this.validatorRegistry.register(validatorId, this.validators[validatorId]);
         }
@@ -129,7 +159,7 @@ export class FormComponent implements OnChanges {
   private setActions() {
     this.actionRegistry.clear();
     if (this.actions) {
-      for (let actionId in this.actions) {
+      for (const actionId in this.actions) {
         if (this.actions.hasOwnProperty(actionId)) {
           this.actionRegistry.register(actionId, this.actions[actionId]);
         }
@@ -139,5 +169,29 @@ export class FormComponent implements OnChanges {
 
   public reset() {
     this.rootProperty.reset(null, true);
+  }
+
+  private setModel(value: any) {
+    if (this.model) {
+      Object.assign(this.model, value);
+    } else {
+      this.model = value;
+    }
+  }
+
+  private onValueChanges(value) {
+    if (this.onChangeCallback) {
+      this.setModel(value);
+      this.onChangeCallback(value);
+    }
+
+    // two way binding is used
+    if (this.modelChange.observers.length > 0) { 
+      if (!this.onChangeCallback) {
+        this.setModel(value);
+      }
+      this.modelChange.emit(value);
+    }
+    this.onChange.emit({value: value});
   }
 }
