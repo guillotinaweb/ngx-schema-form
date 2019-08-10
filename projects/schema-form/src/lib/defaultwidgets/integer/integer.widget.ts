5784c41b9c20184c2e60ce7eabd7e01b219ff9b1
daniele-pecora <daniele.pecora@hsh-berlin.com>
2019-08-10 10:52:10 +0200

    add missing id attribute to defaultswidgets

--- projects/schema-form/src/lib/defaultwidgets/integer/integer.widget.ts
+++ projects/schema-form/src/lib/defaultwidgets/integer/integer.widget.ts
@@ -11,7 +11,8 @@ import { ControlWidget } from '../../widget';
 		{{ schema.title }}
 	</label>
   <span *ngIf="schema.description" class="formHelp">{{schema.description}}</span>
-	<input [attr.readonly]="schema.readOnly?true:null" [name]="name"
+	<input [attr.readonly]="schema.readOnly?true:null" [attr.name]="name"
+	[attr.id]="id"
 	class="text-widget integer-widget form-control" [formControl]="control"
 	[attr.type]="'number'" [attr.min]="schema.minimum" [attr.max]="schema.maximum"
 	[attr.placeholder]="schema.placeholder"
