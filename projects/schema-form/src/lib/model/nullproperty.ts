cd55e8032159987b66ff3cdbfb26b7053ba4bce9
iperevozchikov <iperevozchikov@gmail.com>
2021-11-01 18:22:35 +0700

    Extend allowed primitive types with 'null' value

---
+++ projects/schema-form/src/lib/model/nullproperty.ts
@@ -0,0 +1,25 @@
+import { AtomicProperty } from './atomicproperty';
+import { ValidatorRegistry, PropertyGroup } from '.';
+import { ExpressionCompilerFactory } from '../expression-compiler-factory';
+import { SchemaValidatorFactory } from '../schemavalidatorfactory';
+import { PROPERTY_TYPE_MAPPING } from './typemapping';
+import {ISchema} from './ISchema';
+import { LogService } from '../log.service';
+
+export class NullProperty extends AtomicProperty {
+  fallbackValue() {
+    return null;
+  }
+}
+
+PROPERTY_TYPE_MAPPING.boolean = (
+    schemaValidatorFactory: SchemaValidatorFactory,
+    validatorRegistry: ValidatorRegistry,
+    expressionCompilerFactory: ExpressionCompilerFactory,
+    schema: ISchema,
+    parent: PropertyGroup,
+    path: string,
+    logger: LogService
+) => {
+    return new NullProperty(schemaValidatorFactory, validatorRegistry, expressionCompilerFactory, schema, parent, path, logger);
+};
--- projects/schema-form/src/lib/template-schema/field/field.ts
+++ projects/schema-form/src/lib/template-schema/field/field.ts
@@ -1,5 +1,6 @@
 import { Validator } from '../../model/validator';
 import {ISchema} from '../../model/ISchema';
+import { TUnorderedPair } from '../../model/utils';
 
 export enum FieldType {
   String = 'string',
@@ -8,10 +9,13 @@ export enum FieldType {
   Boolean = 'boolean',
   Integer =  'integer',
   Number = 'number',
+  Null = 'null',
 }
 
-export type TNullableFieldType = [FieldType.String | FieldType.Number | FieldType.Boolean | FieldType.Integer, 'null']
-  | ['null', FieldType.String | FieldType.Number | FieldType.Boolean | FieldType.Integer];
+export type TNullableFieldType = TUnorderedPair<
+  FieldType.Null,
+  FieldType.String | FieldType.Number | FieldType.Boolean | FieldType.Integer
+>;
 
 export interface Field {
   name: string;
